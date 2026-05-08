ed409555fdacc87304c7241a3e769a1b19e60e21
allysson-assuncao <anybody723@proton.me>
2026-05-07 21:33:07 -0300

    feat: commit inicial

---
+++ socket/SERVIDOR.py
@@ -0,0 +1,14 @@
+import socket
+
+host = '127.0.0.1'  # Endereco IP do Servidor
+porta = 5000  # Porta que o Servidor esta
+soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+origem = (host, porta)
+soquete.bind(origem)
+soquete.listen(0)
+while True:
+    conexao, cliente = soquete.accept()
+    print('Concetado por', cliente)
+    mensagem = conexao.recv(1024)
+    print("Cliente ", cliente[0], "Recebida: ", mensagem)
+    conexao.close()
